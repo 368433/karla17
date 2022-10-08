b68715705aeead1f5470cb5cc00f43189cf24575
AI-Orion <amir.ibrhm@gmail.com>
2022-10-08 16:02:16 -0400

    Initial Commit

---
+++ karla17/karla17App.swift
@@ -0,0 +1,20 @@
+//
+//  karla17App.swift
+//  karla17
+//
+//  Created by Amir Mac Pro 2019 on 2022-10-08.
+//
+
+import SwiftUI
+
+@main
+struct karla17App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
